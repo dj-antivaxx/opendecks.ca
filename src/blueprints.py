b5b96cadfbce80d3cd5d10a0b9708b1d32a35caf
dj-antivaxx <dj.antivaxx@gmail.com>
2024-05-29 20:52:18 -0400

    update website for mobile

--- src/blueprints.py
+++ src/blueprints.py
@@ -11,8 +11,8 @@ class EmailForm(Form):
     email = TextAreaField('ur instagram', validators=[
         validators.InputRequired(message="??"), 
         validators.Length(min=2, message="too short!"), 
-        validators.Length(max=40, message="too long!")],
-        render_kw={'rows': 1, 'cols': 40, 'style':'resize:none;background: black;color:white;font-family:Cambria, Cochin, Georgia, Times, "Times New Roman", serif;', 'placeholder': 'enter your instagram link to sign up...'})
+        validators.Length(max=30, message="too long!")],
+        render_kw={'rows': 1, 'cols': 25, 'style':'resize:none;background: black;color:white;font-family:Cambria, Cochin, Georgia, Times, "Times New Roman", serif;', 'placeholder': '@your_instagram_handle'})
     submit = SubmitField('subscribe', render_kw={'style':"border:none; background: lightgrey;"})
 
 @home.route('/', methods=('GET', 'POST'))
